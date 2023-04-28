3bd1e216aaefccd265fc412687e41cc049d221c5
gabrielmoro <gabrielhmoro@gmail.com>
2023-04-28 15:13:55 -0300

    ...

--- energy.r
+++ energy.r
@@ -5,39 +5,77 @@ library(rnaturalearth)
 
 # Energy consumption data
 data <- read.csv("data/energy.csv")
+
+data <- merge(
+    data,
+    subset(
+        read.csv("data/population.csv"),
+        select = c(
+            Entity,
+            Year,
+            Population
+        )
+    ),
+    by = c("Entity", "Year"),
+    all = TRUE
+    )
 data <- data[data$Year == 2021, ]
 
 g <- c(
-    "Antarctica",
     "Asia",
     "America",
     "CIS",
     "Europe",
-    "Eastern Africa",
-    "Western Africa",
+    "Oceania",
     "income",
+    "Small",
     "Middle",
     "OECD",
-    "USSR"
+    "USSR",
+    "regions",
+    "countries"
 )
-for (c in g){
+for (c in g)
     data <- data[!grepl(c, data$Entity), ]
-}
+
 
 data <- data[data$Entity != "Africa", ]
 data <- data[data$Entity != "Africa (BP)", ]
+data <- data[data$Entity != "Africa (UN)", ]
+data <- data[data$Entity != "Eastern Africa", ]
+data <- data[data$Entity != "Eastern Africa (BP)", ]
+data <- data[data$Entity != "Western Africa", ]
+data <- data[data$Entity != "Western Africa (BP)", ]
 
-# TODO: Find a better way to do this
-data$Total.Consumption <- data$Other.Consumption +
-    data$Biofuels.Consumption
-data$Total.Consumption <- data$Total.Consumption + data$Solar.Consumption
-data$Total.Consumption <- data$Total.Consumption + data$Wind.Consumption
-data$Total.Consumption <- data$Total.Consumption + data$Hydro.Consumption
-data$Total.Consumption <- data$Total.Consumption + data$Nuclear.Consumption
-data$Total.Consumption <- data$Total.Consumption + data$Gas.Consumption
-data$Total.Consumption <- data$Total.Consumption + data$Coal.Consumption
-data$Total.Consumption <- data$Total.Consumption + data$Oil.Consumption
+data$Entity[data$Entity == "Democratic Republic of Congo"
+] <- "Democratic Republic of the Congo"
 
+data$Entity[data$Entity == "Sao Tome and Principe"
+] <- "São Tomé and Principe"
+
+data$Renewable.Consumption <- data$Other.Consumption +
+    data$Biofuels.Consumption +
+    data$Solar.Consumption +
+    data$Wind.Consumption +
+    data$Hydro.Consumption
+
+data$Fossil.Fuel.Consumption <- data$Gas.Consumption +
+    data$Coal.Consumption +
+    data$Oil.Consumption
+
+data$Total.Consumption <- data$Renewable.Consumption +
+    data$Fossil.Fuel.Consumption +
+    data$Nuclear.Consumption
+
+# TWh -> KWh (*1000000000)
+data$Consumption.per.capita <- (
+    data$Total.Consumption / data$Population) * 1000000000
+
+# Source relative to total generation
+data$Renewable.Share <- (data$Renewable.Consumption /
+    data$Total.Consumption) * 100
+data$Fossil.Fuel.Share <- (data$Fossil.Fuel.Consumption /
+    data$Total.Consumption) * 100
 data$Biofuels.Share <- (data$Biofuels.Consumption /
     data$Total.Consumption) * 100
 data$Solar.Share <- (data$Solar.Consumption /
@@ -70,6 +108,7 @@ world <- ne_countries(
 
 world$sovereignt[
     world$sovereignt == "United States of America"] <- "United States"
+world <- world[world$sovereignt != "Antarctica", ]
 
 names(world)[names(world) == "sovereignt"] <- "Entity"
 
@@ -93,17 +132,29 @@ world <- subset(
 )
 
 g <- ggplot(data = world) +
-    geom_sf(aes(fill = Total.Share)) +
+    geom_sf(
+        aes(fill = Renewable.Share)) +
     scale_fill_viridis_c(
+        name = "%",
         option = "F",
         # trans = "sqrt"
-        trans = scales::pseudo_log_trans(sigma = 0.5)
+        # trans = scales::pseudo_log_trans(sigma = 0.1)
     ) +
     xlab("Longitude") +
     ylab("Latitude") +
-    theme(legend.position = "bottom", legend.key.width = unit(3.5, "cm")) +
-    ggtitle("Share of energy consumption (TWh) per country")
+    theme(
+        legend.position = "bottom",
+        legend.key.width = unit(3.5, "cm")
+        ) +
+    guides(fill = guide_colorbar(title.position = "top")) +
+    ggtitle("Renewable energy consumed relative to total consumption (2021)")
 
 plot(g)
 
-ggsave("imgs/energy_consumption_share.png", g)
+ggsave(
+    "imgs/energy_consumption_renewable_share.png",
+    g,
+    width = 15,
+    height = 8,
+    dpi = 300
+    )
